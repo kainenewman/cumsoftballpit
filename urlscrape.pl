1a7850e209b31900752edc49ad12a0378991fc7b
c̥ͦu̥ͦm̥ͦs̥ͦo̥ͦf̥ͦt̥ͦ <97250816+cumsoft@users.noreply.github.com>
2022-07-12 19:30:46 -0400

    Create urlscrape.pl

---
+++ urlscrape.pl
@@ -0,0 +1,16 @@
+my $ua
+$ua
+  => 1);
+$ua - > agent("Mozilla/5.0 (X11; Linux
+  x86_64) AppleWebkit / 537.31(KHTML,
+  like Gecko) Chrome / 26.0 .1410 .63
+Safari / 537.31 "); = = LWP::UserAgent - > new;
+LWP::UserAgent - > new(keep_alive $url $request $response = $request - > content; Python ||
+    | =
+    "https://myip.ms/$sitel"; $ua - > get($url);
+    if ($response = ~/
+      local_format ":"(.* ? )
+      "/) {
+        =
+        4 G
+        Format: $localformat\ n ";
